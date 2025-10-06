709defd0310acaac7fd350ebb8adb99c21541ecc
ShirakawaTyu <1400070301@qq.com>
2025-10-06 09:26:06 +0800

    feat: 支持IPV6

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ import (
 //go:embed conf/nodes.json
 var nodesJson []byte
 
+//go:embed conf/nodes_v6.json
+var nodesV6Json []byte
+
 //go:embed conf/banner.txt
 var banner string
 
@@ -35,9 +38,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	nodesV6 := speedtest.TestServer{}
+	err = json.Unmarshal(nodesV6Json, &nodesV6)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Print(banner + `
 1.  中国大陆三网 IPv4 多线程测速
 2.  中国大陆三网 IPv4 单线程测速
+3.  中国大陆三网 IPv6 多线程测速
+4.  中国大陆三网 IPv6 单线程测速
+5.  退出
 请输入您想选择的节点序号: `)
 	var choice int
 	_, err = fmt.Scanf("%d", &choice)
@@ -50,5 +61,15 @@ func main() {
 		speedtest.SpeedTest(nodes, true, banner)
 	case 2:
 		speedtest.SpeedTest(nodes, false, banner)
+	case 3:
+		speedtest.SpeedTest(nodesV6, true, banner)
+	case 4:
+		speedtest.SpeedTest(nodesV6, false, banner)
+	case 5:
+		fmt.Println("退出程序")
+		os.Exit(0)
+	default:
+		fmt.Println("无效输入，退出程序")
+		os.Exit(1)
 	}
 }
